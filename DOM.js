b0dd5fcf03201d53b053de7e763dcb4fa68d4697
vyshnavi-123-max <vyshnavibhavirisetty@gmail.com>
2025-06-25 15:13:02 +0530

    javascript

---
+++ DOM.js
@@ -0,0 +1,18 @@
+//DOM IN JAVASCRIPT
+// The Document Object Model (DOM) is a programming interface for web documents.
+document.title = "DOM in JavaScript";
+document.header="DOM in JavaScript"; 
+//or document.getElementById("header").innerHTML = "DOM in JavaScript";
+document.getElementById("header").style.color = "red"; // Change the color of the element with id "mr" to red
+document.getElementById("header").style.fontSize = "30px"; // Change the font size of the element with id "mr" to 30px
+document.getElementsByClassName("paragraph").innerHTML="She is always sleeping"; // Change the content of the first element with class "paragraph" to "She is always sleeping"
+document.body.style.backgroundColor = "lightblue"; // Change the background color of the body to light blue
+document.getElementsByClassName("ds")[0].style.color = "green"; 
+// Change the color of the first element with class "ds" to green
+document.getElementsByClassName("ds")[1].style.color = "blue";
+document.getElementsByClassName("ds")[2].style.color = "brown"; 
+document.getElementsByClassName("ds")[3].style.color = "purple"; 
+document.getElementsByClassName("ds")[4].style.color = "red"; 
+// Change the color of the first element with class "ds" to green
+// Change the color of the first element with class "ds" to green
+// Change the color of the first element with class "ds" to green 

f0c121b947391dc3b645d6979746e1c5aeb3b288
vyshnavi-123-max <vyshnavibhavirisetty@gmail.com>
2025-07-08 10:31:48 +0530

    Ajio clone

---
+++ app.js
@@ -0,0 +1,6 @@
+setTimeout(()=>{
+    let newText=document.createElement("p");
+    newText.textContent="END OF SEASON SALE";
+    newText.style.color="light blue";
+    document.body.appendChild(newText);
+ },3000);
